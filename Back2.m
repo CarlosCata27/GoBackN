c434b0747be7466636aa4b1d2481e7294d5732cd
Carlos Catarino <carlos_cata27@hotmail.com>
2021-01-11 20:40:19 -0600

    Contadores Listos

--- Back2.m
+++ Back2.m
@@ -1,34 +1,47 @@
 clc;
 clear all;
 NumeroTramas=1000;
-Ventana=[3,5];
+Ventana=3;
+%Ventana=5;
 index=1;
 flag=0;
 flag2=0;
 Tramas=1:NumeroTramas;
-ventana=1;
+NR=[0.01 0.1];
+Contador=zeros(1,4);
+error=0;
+
+Tiempos=zeros(1,NumeroTramas);
+Contador(1)=NumeroTramas;
 while flag==0
     if flag2==0
-    %Transmitting The Frames In A Window
-        for i=1:1:Ventana()
+        %Se transmiten las tramas indicadas por la ventana
+        tic;
+        for i=1:Ventana
             fprintf('Transmitiendo trama %d\n',Tramas(index));
+            Tiempos(index) =toc;
             index=index+1;
+            
         end
         flag2=1;
     end
-    s=randi(10,1,1);
-    if s>3
-        fprintf('PAK of Frame %d Received\n',Tramas(index-Ventana));
-        fprintf('Frame %d Transmitted\n',Tramas(index));
+    Dado=rand(1);
+    if Dado>NR(1)
+        fprintf('ACK recibido de la trama %d \n',Tramas(index-Ventana));
+        
+        fprintf('Transmitiendo trama %d \n',Tramas(index));
         if Tramas(index)==NumeroTramas
             flag=1;
         end
         index=index+1;
     else
-        fprintf('NAK Of Frame %d Received\n',Tramas(index-Ventana));
+        fprintf('Error al recibir ACK  en trama %d\n',Tramas(index-Ventana));
+        Contador(2)=Contador(2)+1;
         for j=0:Ventana-1
-            fprintf('Frame %d Discarded\n',Tramas(index-Ventana+j));
+            fprintf('Descartando trama %d\n',Tramas(index-Ventana+j));
+            Contador(3)=Contador(3)+1;
         end
+        
         index=index-Ventana;
         flag2=0;
     end
@@ -36,17 +49,19 @@ end
 %Last 'W' Frames are dealt seperately
 i=NumeroTramas-Ventana+1;
 while (i<=NumeroTramas)
-    s=randi(10,1,1);
-    if s>4
-        fprintf('Rebicido ACK de trama %d\n',Tramas(i));
+    Dado=rand(1);
+    if Dado>NR(1)
+        fprintf('Recibido ACK de trama %d\n',Tramas(i));
         i=i+1;
+        
     else
         fprintf('Error al recibir ACK de la trama %d\n',Tramas(i));
         for j=i:NumeroTramas
             fprintf('Descartando la trama %d\n',Tramas(j));
         end
         for k=i:NumeroTramas
-            fprintf('Frame %d Transmitted\n',Tramas(k));
+            fprintf('Transmitiendo trama %d\n',Tramas(k));
         end
     end
 end
+Contador(4)=Contador(1)-Contador(2);
