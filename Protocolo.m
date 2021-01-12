8bbe67cfae8534fbc1e846cb6fe210a009caa93d
Carlos Catarino <carlos_cata27@hotmail.com>
2021-01-11 21:53:04 -0600

    Casi terminado

--- Protocolo.m
+++ Protocolo.m
@@ -1,37 +1,26 @@
-clc;
-clear all;
-NumeroTramas=1000;
-Ventana=3;
-%Ventana=5;
-index=1;
-flag=0;
-flag2=0;
-Tramas=1:NumeroTramas;
-NR=[0.01 0.1];
+function [Contador]= Protocolo(NR,Ventana,NumeroTramas)
+Bandera=0;
+Bandera2=0;
 Contador=zeros(1,4);
-error=0;
-
-Tiempos=zeros(1,NumeroTramas);
+index=1;
+Tramas=1:NumeroTramas;
 Contador(1)=NumeroTramas;
-while flag==0
-    if flag2==0
+while Bandera==0
+    if Bandera2==0
         %Se transmiten las tramas indicadas por la ventana
-        tic;
         for i=1:Ventana
             fprintf('Transmitiendo trama %d\n',Tramas(index));
-            Tiempos(index) =toc;
-            index=index+1;
-            
+            index=index+1;   
         end
-        flag2=1;
+        Bandera2=1;
     end
     Dado=rand(1);
-    if Dado>NR(1)
+    if Dado>NR
         fprintf('ACK recibido de la trama %d \n',Tramas(index-Ventana));
         
         fprintf('Transmitiendo trama %d \n',Tramas(index));
         if Tramas(index)==NumeroTramas
-            flag=1;
+            Bandera=1;
         end
         index=index+1;
     else
@@ -41,19 +30,17 @@ while flag==0
             fprintf('Descartando trama %d\n',Tramas(index-Ventana+j));
             Contador(3)=Contador(3)+1;
         end
-        
         index=index-Ventana;
-        flag2=0;
+        Bandera2=0;
     end
 end
-%Last 'W' Frames are dealt seperately
+%Se transmiten por separado las ultimas tramas contenidas en la ventana
 i=NumeroTramas-Ventana+1;
 while (i<=NumeroTramas)
     Dado=rand(1);
-    if Dado>NR(1)
+    if Dado>NR
         fprintf('Recibido ACK de trama %d\n',Tramas(i));
         i=i+1;
-        
     else
         fprintf('Error al recibir ACK de la trama %d\n',Tramas(i));
         for j=i:NumeroTramas
@@ -65,3 +52,4 @@ while (i<=NumeroTramas)
     end
 end
 Contador(4)=Contador(1)-Contador(2);
+end
