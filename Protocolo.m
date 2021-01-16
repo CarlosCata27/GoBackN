81645e800d8966e5e14aa6780159b5c3be55fd6d
Carlos Catarino <carlos_cata27@hotmail.com>
2021-01-16 02:40:36 -0600

    Finish

--- Protocolo.m
+++ Protocolo.m
@@ -1,11 +1,14 @@
-function [Contador]= Protocolo(NR,Ventana,NumeroTramas)
+function [Contador, Tiempo]= Protocolo(NR,Ventana,NumeroTramas)
 Bandera=0;
 Bandera2=0;
 Contador=zeros(1,4);
+Tiempo=zeros(1,NumeroTramas);
 index=1;
 Tramas=1:NumeroTramas;
+time=1;
 Contador(1)=NumeroTramas;
 while Bandera==0
+    tic
     if Bandera2==0
         %Se transmiten las tramas indicadas por la ventana
         for i=1:Ventana
@@ -17,7 +20,6 @@ while Bandera==0
     Dado=rand(1);
     if Dado>NR
         fprintf('ACK recibido de la trama %d \n',Tramas(index-Ventana));
-        
         fprintf('Transmitiendo trama %d \n',Tramas(index));
         if Tramas(index)==NumeroTramas
             Bandera=1;
@@ -33,23 +35,28 @@ while Bandera==0
         index=index-Ventana;
         Bandera2=0;
     end
+    Tiempo(time)=toc;
+    time=time+1;
 end
 %Se transmiten por separado las ultimas tramas contenidas en la ventana
 i=NumeroTramas-Ventana+1;
 while (i<=NumeroTramas)
+    tic
     Dado=rand(1);
     if Dado>NR
         fprintf('Recibido ACK de trama %d\n',Tramas(i));
         i=i+1;
     else
         fprintf('Error al recibir ACK de la trama %d\n',Tramas(i));
+        Contador(2)=Contador(2)+1;
         for j=i:NumeroTramas
             fprintf('Descartando la trama %d\n',Tramas(j));
-        end
-        for k=i:NumeroTramas
-            fprintf('Transmitiendo trama %d\n',Tramas(k));
+            Contador(3)=Contador(3)+1;
+            fprintf('Transmitiendo trama %d\n',Tramas(j));
         end
     end
+    Tiempo(time)=toc;
+    time=time+1;
 end
 Contador(4)=Contador(1)-Contador(2);
 end
